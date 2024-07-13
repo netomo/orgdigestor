24c39ef4f6a8e505f70bc6e9e2ea5a7ccc45a458
Ernesto Mancilla <emancillao@gmail.com>
2024-07-13 17:32:07 -0500

    Added file type and headers validation

--- orgdigestor/api_views.py
+++ orgdigestor/api_views.py
@@ -1,7 +1,9 @@
+import csv
 from http import HTTPMethod
-from rest_framework import viewsets
+from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
 
 from orgdigestor.models import Organization
 from orgdigestor.serializers import OrganizationSerializer, OrganizationsFileDigestSerializer
@@ -20,4 +22,32 @@ class OrganizationViewSet(viewsets.ModelViewSet):
     )
     def digest(self, request):
         serializer = OrganizationsFileDigestSerializer(data=request.data)
-        return self.list(request)
+        if serializer.is_valid():
+            file = serializer.validated_data['file']
+            validation_error = self.validate_csv_file(file)
+
+            if validation_error:
+                return Response({'error': validation_error}, status=status.HTTP_400_BAD_REQUEST)
+
+            # TODO: celery task to digest the file
+            return Response(
+                {'status': 'Aww yeah, file is valid and being processed!'},
+                status=status.HTTP_202_ACCEPTED
+            )
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def validate_csv_file(self, file):
+        required_headers = {'Organization Id', 'Name', 'Country', 'Industry'}
+        try:
+            if not file.name.lower().endswith('.csv'):
+                return "The uploaded file must be a CSV file."
+
+            file.seek(0)
+            reader = csv.DictReader(file.read().decode('utf-8').splitlines())
+            headers = set(reader.fieldnames)
+
+            if not required_headers.issubset(headers):
+                return f"Missing required headers: {required_headers - headers}"
+            return
+        except Exception as e:
+            return str(e)
