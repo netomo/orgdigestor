b4d29dad265a013120f2d8fd541ee3c86c2ec962
Ernesto Mancilla <emancillao@gmail.com>
2024-07-17 10:51:34 -0500

    Updated tasks for better chaining Changed celery pool to gevent Updated the way countries and industries are being queried and created

--- orgdigestor/models.py
+++ orgdigestor/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Country(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
@@ -10,7 +10,7 @@ class Country(models.Model):
 
 class Industry(models.Model):
     name = models.CharField(max_length=100)
-    slug = models.SlugField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
 
     def __str__(self):
         return self.name
--- orgdigestor/tasks.py
+++ orgdigestor/tasks.py
@@ -3,6 +3,8 @@ import csv
 import os
 from dataclasses import dataclass, field, asdict
 from celery import shared_task, group
+from django.utils.text import slugify
+
 from orgdigestor.models import Organization, Country, Industry
 from orgdigestor.serializers import OrganizationSerializer
 
@@ -56,10 +58,23 @@ def write_batch_to_file(batch, headers, batch_number, file_path):
     return new_file_path
 
 
+def map_org_row(row_dict):
+    return {
+        'id': row_dict.get('Organization Id'),
+        'name': row_dict.get('Name'),
+        'country': row_dict.get('Country'),
+        'industry': row_dict.get('Industry'),
+        'website': row_dict.get('Website'),
+        'description': row_dict.get('Description'),
+        'founded': row_dict.get('Founded'),
+        'number_of_employees': row_dict.get('Number of employees'),
+    }
+
+
 @shared_task
 def process_organizations_csv(file_path, rows_per_task):
     """
-    Main orchestration task to process a CSV file with organizations data.
+    Start point task to process a CSV file with organizations' data.
     The process is:
     - Split the file into multiple files with a maximum number of rows.
     - Process each file in a separate task.
@@ -67,88 +82,74 @@ def process_organizations_csv(file_path, rows_per_task):
     - Send a summary report with the results (number of organizations created, updated, etc).
     """
     batch_files = split_csv_file(file_path, rows_per_task)
-    digest_group = group(process_organizations_csv_chunk.s(file_path) for file_path in batch_files)
-    chain = digest_group | process_organizations_csv_summary.s()
-    chain()
-
+    chunks_group = group(process_csv_chunk.s(file_path) for file_path in batch_files)
+    g = chunks_group | sum_reports.s(send_email=True)
+    return g.delay().get()
 
 
 @shared_task
-def process_organizations_csv_chunk(file_path):
+def process_csv_chunk(file_path):
     """
-    Process a CSV file with organizations data. Each row is a dictionary with the following keys:
-    - Organization Id
-    - Name
-    - Website
-    - Country
-    - Description
-    - Founded
-    - Industry
-    - Number of employees
+    Process a CSV file with organizations data.
     """
-    digest_report = OrganizationDigestReport()
 
     with open(file_path, mode='r') as file:
         reader = csv.DictReader(file)
-        countries = {}
-        industries = {}
+        rows_group = group(create_update_organization.s(data=map_org_row(row)) for row in reader)
+        g = rows_group | sum_reports.s()
+        digest_report = g.delay().get()
 
-        for row in reader:
-            organization_id = row.get('Organization Id')
-            name = row.get('Name')
-            website = row.get('Website')
-            country_name = row.get('Country')
-            description = row.get('Description')
-            founded = row.get('Founded')
-            industry_name = row.get('Industry')
-            number_of_employees = row.get('Number of employees')
-
-            try:
-                # Create or update the organization
-                if country_name not in countries:
-                    country, _ = Country.objects.get_or_create(name=country_name)
-                    countries[country_name] = country
-                else:
-                    country = countries[country_name]
-
-                if industry_name not in industries:
-                    industry, _ = Industry.objects.get_or_create(name=industry_name)
-                    industries[industry_name] = industry
-                else:
-                    industry = industries[industry_name]
-
-                serializer = OrganizationSerializer(data={
-                    'id': organization_id,
-                    'name': name,
-                    'website': website,
-                    'country': country.pk,
-                    'industry': industry.pk,
-                    'description': description,
-                    'founded': founded,
-                    'number_of_employees': number_of_employees,
-                })
-                serializer.is_valid(raise_exception=True)
-
-                organization, created = Organization.objects.update_or_create(
-                    id=organization_id,
-                    defaults=serializer.validated_data
-                )
-
-                if created:
-                    digest_report.created += 1
-                else:
-                    digest_report.updated += 1
-            except Exception as e:
-                digest_report.errors += 1
-                digest_report.error_messages.append(str(e))
-
-    # delete the file after processing
     os.remove(file_path)
+    return digest_report
+
+
+@shared_task(bind=True, retry_limit=2, default_retry_delay=5)
+def create_update_organization(self, data):
+    """
+    Create or update an organization based on the data provided.
+    """
+
+    digest_report = OrganizationDigestReport()
+    try:
+        # Create or update the organization
+        organization_id = data.pop('id')
+
+        country_name = data.get('country')
+        country, _ = Country.objects.get_or_create(name=country_name)
+        data['country'] = country.id
+
+        industry_slug = slugify(data.get('industry'))
+        try:
+            industry = Industry.objects.get(slug=industry_slug)
+        except Industry.DoesNotExist:
+            industry = Industry.objects.create(name=data.get('industry'), slug=industry_slug)
+        data['industry'] = industry.id
+
+        serializer = OrganizationSerializer(data=data)
+        serializer.is_valid(raise_exception=True)
+
+        organization, created = Organization.objects.update_or_create(
+            id=organization_id,
+            defaults=serializer.validated_data
+        )
+
+        if created:
+            digest_report.created = 1
+        else:
+            digest_report.updated = 1
+
+    except Exception as e:
+        if self.request.retries < self.max_retries:
+            raise self.retry(exc=e)
+        else:
+            digest_report.errors = 1
+            digest_report.error_messages.append(str(e))
+
     return asdict(digest_report)
 
 
 @shared_task
-def process_organizations_csv_summary(reports):
+def sum_reports(reports, send_email=False):
     """
     Process the reports from each chunk and generate a summary report.
     """
@@ -159,10 +160,13 @@ def process_organizations_csv_summary(reports):
         summary_report.errors += report['errors']
         summary_report.error_messages.extend(report['error_messages'])
 
+    if send_email:
+        print('Sending email...')
+
     print('=== Summary report ===')
     print(f'Created: {summary_report.created}')
     print(f'Updated: {summary_report.updated}')
     print(f'Errors: {summary_report.errors}')
     print('Error messages:')
 
-    return summary_report
+    return asdict(summary_report)
